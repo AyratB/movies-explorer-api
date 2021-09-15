79989c1526ae2fc129e9b14a7869ec496f039abb
Ayrat Burnashev <airatbyr@ya.ru>
2021-09-15 21:08:33 +0300

    add user, movie

---
+++ models/card.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный адрес карточки',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    default: [],
+  }],
+});
+
+module.exports = mongoose.model('card', cardSchema);
---
+++ models/movie.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    min: [0, 'Длительность фильма не может быть отрицательной'],
+  },
+  year: {
+    type: Number,
+    required: true,
+    min: [0, 'Год выпуска должен быть старше 1895 года'],
+    max: [2021, 'Год выпуска должен быть меньше 2021 года'],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный адрес постера',
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный адрес трейлера',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный адрес мини-постера',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
